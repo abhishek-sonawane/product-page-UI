ecbdfb9c411f19520a7ce69dab101f80c8d6f444
abhishek <abhisheksonwane57@gmail.com>
2022-09-21 18:00:25 +0530

    responsive fixes and cart features added

---
+++ src/script.js
@@ -0,0 +1,123 @@
+const arr = [
+  {
+    bigImg: "../images/image-product-1.jpg",
+    thumbnail: "../images/image-product-1-thumbnail.jpg",
+  },
+  {
+    bigImg: "../images/image-product-2.jpg",
+    thumbnail: "../images/image-product-2-thumbnail.jpg",
+  },
+  {
+    bigImg: "../images/image-product-3.jpg",
+    thumbnail: "../images/image-product-3-thumbnail.jpg",
+  },
+  {
+    bigImg: "../images/image-product-4.jpg",
+    thumbnail: "../images/image-product-4-thumbnail.jpg",
+  },
+];
+
+const imgjs = document.getElementById("imgjs");
+const chang = document.getElementById("chang");
+const thumbnail = document.querySelectorAll(".thumbnail");
+const imggallbuttons = document.querySelectorAll(".img-gallery-btns");
+const addToCart = document.querySelector(".add-cart");
+const counterBtn = document.querySelector("#counter-btn");
+const counterDecrementbtn = document.querySelector("#counter-decrement");
+const counterIncrementbtn = document.querySelector("#counter-increment");
+
+counterDecrementbtn.addEventListener("click", () => {
+  counterBtn.value--;
+
+  console.log(counterValue);
+});
+
+counterIncrementbtn.addEventListener("click", () => {
+  counterBtn.value++;
+
+  console.log(counterValue);
+});
+
+let i = 0;
+chang.src = arr[i].bigImg;
+function prevFunc() {
+  if (i > 0) {
+    i--;
+    chang.src = arr[i].bigImg;
+  }
+  console.log(i);
+}
+
+function nextFunc() {
+  if (i < arr.length - 1) {
+    // chang.src=arr[i].bigImg;
+    i++;
+    chang.src = arr[i].bigImg;
+  }
+  console.log(i);
+}
+
+imggallbuttons.forEach((buton) => {
+  buton.addEventListener("click", () => {
+    if (buton.dataset.state == "prev") {
+      // console.log('prev')
+      prevFunc();
+      // console.log(i)
+    } else if (buton.dataset.state == "next") {
+      // console.log('next')
+      nextFunc();
+      // console.log(i)
+    }
+  });
+});
+
+thumbnail.forEach((thum) => {
+  //    console.log(thum.src)
+  thum.addEventListener("click", (e) => {
+    // var newse = thum.src.replace('-thumbnail','');
+    const trimmed = `..${thum.src.slice(21)}`;
+    console.log(trimmed);
+    const th = arr.find((item) => {
+      return item.thumbnail === trimmed;
+    });
+    i = arr.indexOf(th);
+    console.log("the one in array is", th);
+    // chang.src = newse
+    chang.src = arr[i].bigImg;
+  });
+});
+
+function toggleCart() {
+  document.querySelector(".hi").classList.toggle("hidden");
+  if (
+    !document
+      .querySelector(".hi")
+      .firstElementChild.classList.contains("conditional")
+  ) {
+    document.querySelector(".hi").firstElementChild.innerHTML =
+      "your cart is empty";
+  }
+}
+
+function addProdToCart() {
+  document.querySelector(".hi").firstElementChild.classList.add("conditional");
+  document.querySelector(
+    ".hi"
+  ).firstElementChild.innerHTML = `  <div class="cart-item">
+        <img width="50px" style="border-radius:7px;" src="../images/image-product-1-thumbnail.jpg" alt="">
+        <div>
+         <p>fall limited edition sneakers</p>
+         <p>125$ x ${counterBtn.value}</p>
+        </div>
+         <a class='deletebtn' href="#"><img src="../images/icon-delete.svg" alt=""> </a>
+     </div>
+     <button  id="cart-btn">checkout</button>`;
+
+  document.querySelector(".deletebtn").addEventListener("click", () => {
+    console.log("delet");
+    document.querySelector(".hi").firstElementChild.innerHTML = "";
+  });
+}
+
+document.querySelector(".nav-second-sec").addEventListener("click", toggleCart);
+addToCart.addEventListener("click", addProdToCart);
